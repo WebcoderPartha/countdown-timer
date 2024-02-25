89be1f6c06e7c1a95f8a33810d6bd2fe28629038
Parthadeb Mondal <Parthadev76@gmail.com>
2024-02-25 17:06:30 +0600

    Count down added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  const countDown = new Date('02/27/2024').getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+      }, 0)
+  }());
